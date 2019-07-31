85ae329d788ef97eb6f2a2099c72173a0c5e11ab
Zefram Lou <zeframlou@gmail.com>
2019-07-31 13:40:30 +0800

    added cname to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 ng build --prod --base-href "https://betoken.fund/"
+echo "betoken.fund" > dist/betoken-landing-page/CNAME
 npx angular-cli-ghpages --dir=dist/betoken-landing-page --repo=git@github.com:Betoken/Betoken.github.io.git --branch=master
