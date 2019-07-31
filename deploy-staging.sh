51c011c7afb3f548889c72c23907f9fd1625b41e
Zefram Lou <zeframlou@gmail.com>
2019-07-31 13:09:26 +0800

    added deployment scripts

---
+++ deploy-staging.sh
@@ -0,0 +1,2 @@
+ng build --prod --base-href "https://betoken.fund/landing-page-staging/"
+npx angular-cli-ghpages --dir=dist/betoken-landing-page --repo=git@github.com:Betoken/landing-page-staging.git --branch=master
---
+++ deploy.sh
@@ -0,0 +1,2 @@
+ng build --prod --base-href "https://betoken.fund/"
+npx angular-cli-ghpages --dir=dist/betoken-landing-page --repo=git@github.com:Betoken/Betoken.github.io.git --branch=master
