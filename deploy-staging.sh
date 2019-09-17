b890ee73a81f48f5d9cda32decfef886fde18982
Zefram Lou <zeframlou@gmail.com>
2019-09-17 18:01:01 +0800

    finished deployment process

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,1 +1,8 @@
-ng deploy --repo=git@github.com:Betoken/landing-page-staging.git --branch=master --base-href=https://betoken.fund/landing-page-staging/
+ng build --prod --base-href=/landing-page-staging/
+ng build --configuration=ja --base-href=/landing-page-staging/ja/
+ng build --configuration=pt --base-href=/landing-page-staging/pt/
+ng build --configuration=zh-Hans --base-href=/landing-page-staging/zh-Hans/
+
+node addMetaTags.js
+
+ng deploy --repo=git@github.com:Betoken/landing-page-staging.git --branch=master --no-build
