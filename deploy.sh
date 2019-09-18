c2ecb70ad90aa428e295163e4b07e35416133030
Zefram Lou <zeframlou@gmail.com>
2019-09-18 15:14:25 +0800

    added production deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
-ng deploy --repo=git@github.com:Betoken/Betoken.github.io.git --branch=master --base-href=https://betoken.fund/ --cname=betoken.fund
+ng build --prod --base-href=/
+ng build --configuration=ja --base-href=/ja/
+ng build --configuration=pt --base-href=/pt/
+ng build --configuration=zh-Hans --base-href=/zh-Hans/
+
+node addMetaTags.js
+
+ng deploy --repo=git@github.com:Betoken/Betoken.github.io.git --branch=master --cname=betoken.fund --no-build
